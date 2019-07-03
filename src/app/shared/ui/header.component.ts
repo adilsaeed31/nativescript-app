3dc0cd28e9c04c3569dc47cab443d2585f8e75c6
Adil <adil@Mohds-iMac.hotspot>
2019-07-03 08:38:00 +0300

    Added search icon

--- src/app/shared/ui/header.component.ts
+++ src/app/shared/ui/header.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, Input } from "@angular/core";
-import { RadSideDrawer } from "nativescript-ui-sidedrawer";
-import * as app from "tns-core-modules/application";
+import { Component, OnInit, Input } from "@angular/core"
+import { RadSideDrawer } from "nativescript-ui-sidedrawer"
+import * as app from "tns-core-modules/application"
 
 @Component({
     selector: "pc-header-bar",
@@ -8,19 +8,23 @@ import * as app from "tns-core-modules/application";
     moduleId: module.id
 })
 export class HeaderComponent implements OnInit {
-    @Input() title: string = "PHCC Conference";
-    headerTitle: string = "";
+    @Input() title: string = "PHCC Conference"
+    headerTitle: string = ""
     constructor() {
         // Use the component constructor to inject providers.
     }
 
     ngOnInit(): void {
         // Init your component properties here.
-        this.headerTitle = this.title;
+        this.headerTitle = this.title
     }
 
     onDrawerButtonTap(): void {
-        const sideDrawer = <RadSideDrawer>app.getRootView();
-        sideDrawer.showDrawer();
+        const sideDrawer = <RadSideDrawer>app.getRootView()
+        sideDrawer.showDrawer()
+    }
+
+    onSearchActionTap(): void {
+        console.log("Search Icon Tap")
     }
 }
